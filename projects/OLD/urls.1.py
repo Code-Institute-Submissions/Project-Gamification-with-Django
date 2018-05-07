92f12c968e41b5cc5597033cd466d59ac3e55b9f
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-05-07 10:26:14 +0000

    Add project view

--- projects/OLD/urls.1.py
+++ projects/OLD/urls.1.py
@@ -1,8 +1,7 @@
 from django.urls import path, include
-from .views import all_projects, project_details, propose_project
+from .views import all_projects, project_details
 
 urlpatterns = [
     path('', all_projects, name="projects"),
     path('project_details/<int:pk>', project_details, name='project_details'),
-    path('propose_project/', propose_project, name='propose_project'),
     ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
+from .forms import ProposeProjectForm
+from django.urls import reverse
 
 
 def all_projects(request):
@@ -9,3 +11,29 @@ def all_projects(request):
 def project_details(request, pk):
     project = Project.objects.get(id=pk)
     return render(request, 'project_details.html', {'project': project})
+
+def propose_project(request):
+    
+    if request.method == 'POST':
+       
+       form = ProposeProjectForm(request.POST)
+       
+       if form.is_valid():
+           name = form.cleaned_data['name']
+           description = form.cleaned_data['description']
+           project_manager = form.cleaned_data['project_manager']
+           budget = form.cleaned_data['budget']
+           
+           Project.objects.create(
+               name = name,
+               description = description,
+               project_manager = project_manager,
+               budget = budget
+               ).save()
+
+           return redirect(reverse('projects'))
+       
+    else:
+        form = ProposeProjectForm()
+        
+    return render (request, 'propose_project.html', {'form': form })    
