23f7e2265a03b0d3357f40af5328084a7ff1349a
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-06-06 19:53:48 +0000

    travis test

--- projects/tests.py
+++ projects/tests.py
@@ -1,15 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-
-
-if innovation_ratio > 5:
-        statement_2 = get_object_or_404(GamificationAdvice, name="innovation++")  
-    elif 0 < innovation_ratio <= 5:
-        statement_2 = get_object_or_404(GamificationAdvice, name="innovation+") 
-    elif -5 <= innovation_ratio < 0:
-        statement_2 = get_object_or_404(GamificationAdvice, name="innovation-")
-    elif innovation_ratio < -5:
-        statement_2 = get_object_or_404(GamificationAdvice, name="innovation--")    
-    else:
-        statement_2 = get_object_or_404(GamificationAdvice, name="innovation0")
