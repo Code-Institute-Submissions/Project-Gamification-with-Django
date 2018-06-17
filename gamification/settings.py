438c033c6a14ab1de66cc41278e5c76e031c15e6
Lukasz_Malucha <lucasmalucha@gmail.com>
2018-06-17 21:35:45 +0000

    heroku

--- gamification/settings.py
+++ gamification/settings.py
@@ -173,7 +173,7 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)      ## comment out for heroku deployment
-MEDIA_URL = '/media/'                                                         ## activate for heroku deployment   
+MEDIA_URL = '/media/'                                                         ## activate for heroku deployment  
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
